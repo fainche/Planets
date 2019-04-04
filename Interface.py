32c7f985babaf8a0f44d88ff1ddbece51bc24151
Gregory Bryk <gbryk1@gmail.com>
2019-04-04 23:35:56 +0200

    live update of pygame view

---
+++ Interface.py
@@ -0,0 +1,107 @@
+import tkinter as tk
+import numpy as np
+
+from Simulation import Simulation
+from Visualization import Visualization
+
+
+class Interface:
+    def __init__(self):
+        self.init_TkWindow()
+        self.v = Visualization()
+        self.v.draw((100, 100))
+        tk.mainloop()
+
+    def update_planet(self, _):
+        x = eval(self.vals["xtext"].get())
+        y = eval(self.vals["ytext"].get())
+        pos = np.array((x, y))
+        vx = eval(self.vals["vxtext"].get())
+        vy = eval(self.vals["vytext"].get())
+        vel = np.array((vx, vy)) * 10
+        r1 = eval(self.vals["r1text"].get())
+        r2 = eval(self.vals["r2text"].get())
+        rad = np.array((r1, r2))
+        self.v.draw(pos, vel + pos, rad)
+
+    def init_TkWindow(self):
+        self.window = tk.Tk()
+        self.window.geometry("500x800")
+        self.window.title("Dane")
+        text = tk.StringVar()
+        label = tk.Label(self.window, textvariable=text, padx=100, pady=50, font=("Times New Roman", 20))
+        text.set("Wprowadź dane symulacji")
+        label.pack()  # podpinanie kontrolki pod okno
+
+        self.vals = {
+            "vxtext": tk.StringVar(),
+            "vytext": tk.StringVar(),
+            "xtext": tk.StringVar(),
+            "ytext": tk.StringVar(),
+            "ttext": tk.StringVar(),
+            "m1text": tk.StringVar(),
+            "m2text": tk.StringVar(),
+            "r1text": tk.StringVar(),
+            "r2text": tk.StringVar()}
+
+        for v in self.vals.values():
+            v.set(100)
+
+        tk.Label(self.window, text="Położenie planety, współrzędna x").pack()
+        start_position_x = tk.Entry(self.window, textvariable=self.vals["xtext"], width=40)
+        start_position_x.pack()
+        tk.Label(self.window, text="Położenie planety, współrzędna y").pack()
+        start_position_y = tk.Entry(self.window, textvariable=self.vals["ytext"], width=40)
+        start_position_y.pack()
+
+        tk.Label(self.window, text="Prędkość początkowa Vx:").pack()
+        velocity_x = tk.Entry(self.window, textvariable=self.vals["vxtext"], width=40)
+        velocity_x.pack()
+        tk.Label(self.window, text="Prędkość początkowa Vy:").pack()
+        velocity_y = tk.Entry(self.window, textvariable=self.vals["vytext"], width=40)
+        velocity_y.pack()
+
+        tk.Label(self.window, text="Krok czasu:").pack()
+        time_step = tk.Entry(self.window, textvariable=self.vals["ttext"], width=40)
+        time_step.pack()
+        tk.Label(self.window, text="Masa nieruchomej planety").pack()
+        mass1 = tk.Entry(self.window, textvariable=self.vals["m1text"], width=40)
+        mass1.pack()
+        tk.Label(self.window, text="Masa ruchomej planety").pack()
+        mass2 = tk.Entry(self.window, textvariable=self.vals["m2text"], width=40)
+        mass2.pack()
+        tk.Label(self.window, text="Promień nieruchomej planety").pack()
+        radiation1 = tk.Entry(self.window, textvariable=self.vals["r1text"], width=40)
+        radiation1.pack()
+        tk.Label(self.window, text="Promień ruchomej planety").pack()
+        radiation2 = tk.Entry(self.window, textvariable=self.vals["r2text"], width=40)
+        radiation2.pack()
+        break_label = tk.Label(self.window, text=" ")
+        break_label.pack()
+
+        start_position_x.focus_set()
+        run = tk.Button(self.window, text="Run", width=20, command=self.start_simulation)
+        run.pack()
+
+        self.window.bind('<KeyRelease>', self.update_planet)
+
+    def start_simulation(self):
+        v1 = eval(self.vals["vxtext"].get()) or 0
+        v2 = eval(self.vals["vytext"].get()) or 0
+        r1 = eval(self.vals["r1text"].get()) or 0
+        r2 = eval(self.vals["r2text"].get()) or 0
+        sp1 = eval(self.vals["xtext"].get()) or 0
+        sp2 = eval(self.vals["ytext"].get()) or 0
+        ts = eval(self.vals["ttext"].get()) or 0
+        m1 = eval(self.vals["m1text"].get()) or 0
+        m2 = eval(self.vals["m2text"].get()) or 0
+
+        s = Simulation((float((sp1)), float((sp2))), (float((v1)), float((v2))), float((ts)),
+                       int(m1), int(m2), float((r1)), float((r2)))
+
+        for i in s:
+            self.v.draw(i, rad=(r1, r2))
+
+
+if __name__ == "__main__":
+    i = Interface()
--- Visualization.py
+++ Visualization.py
@@ -1,14 +1,11 @@
 import pygame
-import tkinter as tk
-
-from Simulation import Simulation
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 500
 
 
-class Visualization:
-    def __init__(self, r1, r2):
+class Visualization():
+    def __init__(self, r1=10, r2=10):
         self.r1 = r1
         self.r2 = r2
         pygame.init()
@@ -17,16 +14,23 @@ class Visualization:
         pygame.display.set_caption("Planety")
         self.clock = pygame.time.Clock()
 
-    def draw(self, planet_position):
+    def draw(self, planet_position, arrow_end = None, rad=None):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 raise SystemExit()
         self.screen.fill((255, 255, 255))
 
+        r1 = rad[0] if rad is not None else self.r1
+        r2 = rad[1] if rad is not None else self.r2
+
         cords = self.__convert_coords(planet_position)
-        pygame.draw.circle(self.screen, (200, 0, 0), (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2)), self.r1)
-        pygame.draw.circle(self.screen, (0, 0, 20), cords, self.r2)
-        # self.clock.tick(60)
+        pygame.draw.circle(self.screen, (200, 0, 0), (int(SCREEN_WIDTH / 2), int(SCREEN_HEIGHT / 2)), r1)
+        pygame.draw.circle(self.screen, (0, 0, 20), cords, r2)
+        if arrow_end is not None:
+            endarr = self.__convert_coords(arrow_end)
+            pygame.draw.aaline(self.screen, (255, 0, 0), cords, endarr, True)
+
+        self.clock.tick(60)
         # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
 
@@ -37,88 +41,6 @@ class Visualization:
         return x, y
 
 
-def run_visualisation():
-    v1 = velocity_x.get()
-    v2 = velocity_y.get()
-    r1 = radiation1.get() or 0
-    r2 = radiation2.get() or 0
-    sp1 = start_position_x.get()
-    sp2 = start_position_y.get()
-    ts = time_step.get()
-    m1 = mass1.get()
-    m2 = mass2.get()
-
-    v = Visualization(int(r1), int(r2))
-    s = Simulation((float(eval(sp1)), float(eval(sp2))), (float(eval(v1)), float(eval(v2))), float(eval(ts)),
-                   int(m1), int(m2), float(eval(r1)), float(eval(r2)))
-
-    for i in s:
-        v.draw(i)
-
 
 if __name__ == '__main__':
-    window = tk.Tk()
-    window.geometry("500x800")
-    window.title("Dane")
-    text = tk.StringVar()
-    label = tk.Label(window, textvariable=text, padx=100, pady=50, font=("Times New Roman", 20))
-    text.set("Wprowadź dane symulacji")
-    label.pack()  # podpinanie kontrolki pod okno
-
-    vals = {
-        "vxtext": tk.StringVar(),
-        "vytext": tk.StringVar(),
-        "xtext": tk.StringVar(),
-        "ytext": tk.StringVar(),
-        "ttext": tk.StringVar(),
-        "m1text": tk.StringVar(),
-        "m2text": tk.StringVar(),
-        "r1text": tk.StringVar(),
-        "r2text": tk.StringVar()}
-
-    velocity_x_desc = tk.Label(window, text="Prędkość początkowa Vx:").pack()
-    velocity_x = tk.Entry(window, textvariable=vals["vxtext"], width=40)
-    velocity_x.pack()
-
-    label1 = tk.Label(window, textvariable=vals["vxtext"])
-    label1.pack()  # podpinanie kontrolki pod okno
-
-    velocity_y_desc = tk.Label(window, text="Prędkość początkowa Vy:").pack()
-    velocity_y = tk.Entry(window, textvariable=vals["vytext"], width=40)
-    velocity_y.pack()
-
-    position_x_desc = tk.Label(window, text="Położenie planety nr 2, współrzędna x").pack()
-    start_position_x = tk.Entry(window, textvariable=vals["xtext"], width=40)
-    start_position_x.pack()
-
-    position_y_desc = tk.Label(window, text="Położenie planety nr 2, współrzędna y").pack()
-    start_position_y = tk.Entry(window, textvariable=vals["ytext"], width=40)
-    start_position_y.pack()
-
-    time_step_desc = tk.Label(window, text="Krok czasu:").pack()
-    time_step = tk.Entry(window, textvariable=vals["ttext"], width=40)
-    time_step.pack()
-
-    mass1_desc = tk.Label(window, text="Masa nieruchomej planety").pack()
-    mass1 = tk.Entry(window, textvariable=vals["m1text"], width=40)
-    mass1.pack()
-
-    mass2_desc = tk.Label(window, text="Masa ruchomej planety").pack()
-    mass2 = tk.Entry(window, textvariable=vals["m2text"], width=40)
-    mass2.pack()
-
-    radiation1_desc = tk.Label(window, text="Promień nieruchomej planety").pack()
-    radiation1 = tk.Entry(window, textvariable=vals["r1text"], width=40)
-    radiation1.pack()
-
-    radiation2_desc = tk.Label(window, text="Promień ruchomej planety").pack()
-    radiation2 = tk.Entry(window, textvariable=vals["r2text"], width=40)
-    radiation2.pack()
-
-    break_label = tk.Label(window, text=" ")
-    break_label.pack()
-
-    run = tk.Button(window, text="Run", width=20, command=run_visualisation)
-    run.pack()
-
-    tk.mainloop()
+    pass
